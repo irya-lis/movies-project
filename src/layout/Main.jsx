acc5c8f473b9b55e6161f270d473c948ca73fdfa
Irina Rakova <lis.ovskaya1119@gmail.com>
2023-02-28 13:35:59 +0300

    add Header, Footer

---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <main className="container content">
+            hello from movies App
+        </main>
+    )
+}
+
+export default Main;
