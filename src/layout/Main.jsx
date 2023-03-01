c3f434c5b71fe84dafe7f787f5c031fbe0e07293
Irina Rakova <lis.ovskaya1119@gmail.com>
2023-03-01 12:40:38 +0300

    add component movie

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import Movies from "../components/Movies";
 
-const Main = () => {
-    return (
-        <main className="container content">
-            hello from movies App
-        </main>
-    )
+class Main extends React.Component {
+    state = {
+        movies: []
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=ad86ae70&s=Wilfred')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+        const {movies} = this.state
+        return (
+            <main className="container content">
+                {movies.length ? (
+                    <Movies movies={this.state.movies}/>
+                ) : <h3>Loading...</h3>
+                }
+            </main>
+        )
+    }
 }
 
 export default Main;
