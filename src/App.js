1fb0795d755ea595ee41ba1ef051b72029154f8e
irya-lis <98985061+irya-lis@users.noreply.github.com>
2023-03-06 18:18:30 +0300

    Revert "Feature/movie5"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import Header from "./layout/Header";
-import Footer from "./layout/Footer";
-import Main from "./layout/Main";
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Main/>
-    <Footer/>
-    </>
+    <div className="App">
+
+    </div>
   );
 }
 
