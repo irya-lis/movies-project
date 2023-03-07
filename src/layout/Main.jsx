ea4cf80bcceb70486dccfb237336676b89e284d9
Irina Rakova <lis.ovskaya1119@gmail.com>
2023-03-07 12:07:28 +0300

    .

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Movies from "../components/Movies";
 import Preloader from "../components/Preloader";
 import Search from "../components/Search";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends React.Component {
     state = {
@@ -10,14 +11,14 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=ad86ae70&s=Liar')
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=Liar`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
     }
 
     searchMovies = (str, type = "all") => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=ad86ae70&s=${str}${type !== "all" ? `&type=${type}` : ''}`)
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== "all" ? `&type=${type}` : ''}`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
     }
